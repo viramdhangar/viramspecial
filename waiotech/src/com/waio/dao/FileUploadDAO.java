9315448cd5af865a7a942ce4d42759de7e44a429
viramdhangar <viram.dhangar@gmail.com>
2016-12-23 13:27:37 +0530

    remove

--- waiotech/src/com/waio/dao/FileUploadDAO.java
+++
@@ -1,7 +0,0 @@
-package com.waio.dao;
-
-import com.waio.dto.UploadFile;
-
-public interface FileUploadDAO {
-	void save(UploadFile uploadFile);
-}
--- waiotech/src/com/waio/dto/UploadedFile.java
+++
@@ -1,8 +0,0 @@
-package com.waio.dto;
-public class UploadedFile {
- 
-    public int length;
-    public byte[] bytes;
-    public String name;
-    public String type;
-}
